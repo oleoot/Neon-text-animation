4e835be8b58c613e531b8190f69e9cbb51796080
oleoot <oleoot28@gmail.com>
2020-03-05 20:03:50 +0200

    Neon text animation

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $("#neonthis").novacancy({
+        "reblinkProbability": 0.6,
+        "blinkMin": 0.1,
+        "blinkMax": 0.2,
+        "loopMin": 1,
+        "loopMax": 2,
+        "color": "red",
+        "glow": ["0 0 80px red", "0 0 30px red", "0 0 6px red"],
+        "off": 3,
+        "blink": 0,
+        "autoOn": true
+    })
+
+})
